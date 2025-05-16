f65f86f4c301329a6b8a7a58e1484fbcc2250394
HallyssonAristides <hallysson.ari@gmail.com>
2025-05-16 19:23:16 -0300

    Initial

---
+++ palavras.py
@@ -0,0 +1,23 @@
+palavras = [
+    'Abacaxi',
+    'Abóbora',
+    'Peixe',
+    'batata',
+    'Quejo',
+    'Zumbi',
+    'Computador',
+    'Mouse',
+    'Teclado',
+    'Fio',
+    'Monitor',
+    'Bola',
+    'Microfone',
+    'Livro',
+    'Fone',
+    'Telefone',
+    'Flor',
+    'Câmera',
+    'Suporte',
+    'Espada',
+    'Machado'
+]
